f4a04dabc57fb93886cf3b31f593301b014310db
sehn-one <isehn1219@gmail.com>
2022-05-07 20:17:54 +0900

    물오리 클래스 구현 + 테스트

---
+++ src/com/onestep/ch01/MiniDuckSimulator.java
@@ -0,0 +1,9 @@
+package com.onestep.ch01;
+
+public class MiniDuckSimulator {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.performFly();
+        mallard.performQuack();
+    }
+}
